5c301b9d486ed590b6cda6e4b69ad61b15fc8f28
unknown <prashantkumarsingh934@gmail.com>
2023-09-30 19:44:36 +0530

    First Project

--- Interfacing button with Arduino.cpp
+++ Interfacing button with Arduino.cpp
@@ -1,5 +1,6 @@
 //be sure to add header file before execution if you are running this code in other than tinker cad
 // link of the project - https://www.tinkercad.com/things/kb5RoZgA5o8?sharecode=wBqzJUSGnh0Ykfs0pZ7xFn0G5vXzw1L-ixCGvZriYG4
+// Pin Diagram is avilable in Readme.md File
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
